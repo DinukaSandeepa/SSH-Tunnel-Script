5b0a6a0c441f05d693224b9b3372752c943a8c12
dinuka340 <60549728+dinuka340@users.noreply.github.com>
2021-01-15 09:26:26 +0530

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -69,10 +69,10 @@ cat >> "$FILE2" <<EOL
 <b><font color="#E74C3C">&#128683; NO OVER DOWNLOADING &#128683</font></b><br>
 <b><h4><font color="#E74C3C">&#9888;&#65039; අවවාදයයි! &#9888;&#65039;</font>
 <br>
-<font color="#C0392B"> Torrent සදහා භාවිතා කිරීමෙන් වළකින්න.<br> Expire Date : 2021 March 26</font></h4></b>
+<font color="#C0392B"> Torrent සදහා භාවිතා කිරීමෙන් වළකින්න.<br> Expire Date : 2021 Feb 26</font></h4></b>
 <b><h4><font color="#008AD7">&#128304; DINUKA CREATION &#128304;</font>
 <br>
-<font color="#66ff6d">&#127473;&#127472; WhatsApp : +1 (701) 969-9699 &#127473;&#127472;</font></h4></b>
+<font color="#66ff6d">&#127473;&#127472; Telegram : @iAm_Jarvis &#127473;&#127472;</font></h4></b>
 </body>
 </html>
 EOL
