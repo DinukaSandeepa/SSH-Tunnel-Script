3910d20136a45cd89a6dc48c9485cd7b3d555cce
dinuka340 <60549728+dinuka340@users.noreply.github.com>
2021-05-07 09:35:50 +0530

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -60,7 +60,7 @@ echo -e "\e[96mCreating dropbear banner\e[0m"
 cat >> "$FILE2" <<EOL
 <html>
 <body>
-<b><h3><font color="#17A589">&#127480;&#127468; JARVIS SG SERVER &#127480;&#127468;</font>
+<b><h3><font color="#17A589">&#127480;&#127468; BONGA SG SERVER &#127480;&#127468;</font>
 <br></b><br><b><font color="#E74C3C">&#128683; NO SPAM &#128683</font></b><br>
 <b><font color="#E74C3C">&#128683; NO DDOS &#128683</font></b><br>
 <b><font color="#E74C3C">&#128683; NO HACKING &#128683</font></b><br>
@@ -69,10 +69,10 @@ cat >> "$FILE2" <<EOL
 <b><font color="#E74C3C">&#128683; NO OVER DOWNLOADING &#128683</font></b><br>
 <b><h4><font color="#E74C3C">&#9888;&#65039; අවවාදයයි! &#9888;&#65039;</font>
 <br>
-<font color="#C0392B"> Torrent සදහා භාවිතා කිරීමෙන් වළකින්න.<br> Expire Date : 2021 Feb 26</font></h4></b>
-<b><h4><font color="#008AD7">&#128304; DINUKA CREATION &#128304;</font>
+<font color="#C0392B"> Torrent සදහා භාවිතා කිරීමෙන් වළකින්න.<br> Expire Date : 2021 July 06</font></h4></b>
+<b><h4><font color="#008AD7">&#128304; Bonga Bro &#128304;</font>
 <br>
-<font color="#66ff6d">&#127473;&#127472; Telegram : @iAm_Jarvis &#127473;&#127472;</font></h4></b>
+<font color="#66ff6d">&#127473;&#127472; Telegram : @RamiruVindiya &#127473;&#127472;</font></h4></b>
 </body>
 </html>
 EOL
